39540224786b21d73ca2f5c10b1b67db2cf9598a
Victor <victorstudios28@gmail.com>
2022-04-06 18:59:40 -0700

    chore: worked on saving input as object and pushing to array:

--- main.js
+++ main.js
@@ -40,39 +40,51 @@ function message() {
 //     - in the resulting String. If omitted, the array elements are separated
 //     - with a comma.
 // - innerHTML returns the HTML inside of the getElementById
+const tableElement = document.getElementById('bookTable'); 
+//Separated so that we could reset the table every render
 
-books.forEach((element) => {
-  const tableRow = document.createElement('tr');
-  const tableDataTitle = document.createElement('td');
-  const tableDataAuthor = document.createElement('td');
-  const tableDataRead = document.createElement('td');
-  const tableDataFav = document.createElement('td');
+function renderTable() {
+  tableElement.innerHTML = ''; //On every call of renderTable(), the table gets cleared
+  books.forEach((element) => {
+    const tableRow = document.createElement('tr');
+    const tableDataTitle = document.createElement('td');
+    const tableDataAuthor = document.createElement('td');
+    const tableDataRead = document.createElement('td');
+    const tableDataFav = document.createElement('td');
 
-  tableDataTitle.innerText = element.title;
-  tableDataAuthor.innerText = element.author;
+    tableDataTitle.innerText = element.title;
+    tableDataAuthor.innerText = element.author;
 
-  tableDataRead.innerHTML = '<input type="checkbox" ' + (element.read ? 'checked=true' : '') + '/>';
-  tableDataFav.innerHTML = '<input type="checkbox" ' + (element.read ? 'checked=true' : '') + '/>';
+    tableDataRead.innerHTML =
+      '<input type="checkbox" ' + (element.read ? 'checked=true' : '') + '/>';
+    tableDataFav.innerHTML =
+      '<input type="checkbox" ' + (element.favorite ? 'checked=true' : '') + '/>';
 
-  tableRow.appendChild(tableDataTitle);
-  tableRow.appendChild(tableDataAuthor);
-  tableRow.appendChild(tableDataRead);
-  tableRow.appendChild(tableDataFav);
+    tableRow.appendChild(tableDataTitle);
+    tableRow.appendChild(tableDataAuthor);
+    tableRow.appendChild(tableDataRead);
+    tableRow.appendChild(tableDataFav);
+    tableElement.appendChild(tableRow);
+  });
+}
+renderTable(); //In order so that on load, the table could be populated
 
-  document.getElementById('testList').appendChild(tableRow);
+const formElement = document.getElementById('form');
+// Attaches event handler to the specifyed element.
+formElement.addEventListener('submit', function (event) {
+  event.preventDefault();
+  // Cancels the defаult behavior. If it's a link, the default is to open it.
+  // This prevents it
+  const values = {
+    title: formElement['title'].value,
+    author: formElement['author'].value,
+    read: formElement['read'].checked,
+    favorite: formElement['favorite'].checked,
+  };
+  books.push(values);
+  renderTable();
 });
 
-const formElement = document.getElementById('form')
-// Attaches event handler to the specifyed element.
-formElement.addEventListener("submit", function(event){
-  event.preventDefault(); 
-  // Cancels the defаult behavior. If it's a link, the default is to open it. 
-  // This prevents it
-  console.log(formElement["title"].value);
-  console.log(formElement["author"].value);
-  console.log(formElement["read"].checked);
-  console.log(formElement["favorite"].checked);
-})
 // document.getElementById('books').innerHTML = books
 //   .map(
 //     (book) =>
