f2fa8c7f88793705c18c36d2db3f2ed3d2fc1c5d
Victor <victorstudios28@gmail.com>
2022-05-20 18:12:45 -0700

    chore: delete work

--- main.js
+++ main.js
@@ -14,14 +14,19 @@ function message() {
 const tableElement = document.getElementById('bookTable');
 //Separated so that we could reset the table every render
 
+let numberid = 0;
+
 function renderTable() {
   tableElement.innerHTML = ''; //On every call of renderTable(), the table gets cleared
   books.forEach((element) => {
+    // element.id = numberid;
+    // numberid = numberid + 1;
     const tableRow = document.createElement('tr');
     const tableDataTitle = document.createElement('td');
     const tableDataAuthor = document.createElement('td');
     const tableDataRead = document.createElement('td');
     const tableDataFav = document.createElement('td');
+    const deleteButton = document.createElement('button');
 
     tableDataTitle.innerText = element.title;
     tableDataAuthor.innerText = element.author;
@@ -32,11 +37,20 @@ function renderTable() {
       '<input type="checkbox" ' +
       (element.favorite ? 'checked=true' : '') +
       '/>';
+    deleteButton.innerHTML = '<button>Delete</button>';
+
+    deleteButton.addEventListener('click', function (event) {
+      event.preventDefault();
+      books = books.filter(book => book.id != element.id);
+      window.localStorage.setItem('localBooksData', JSON.stringify(books));
+      renderTable();
+    });
 
     tableRow.appendChild(tableDataTitle);
     tableRow.appendChild(tableDataAuthor);
     tableRow.appendChild(tableDataRead);
     tableRow.appendChild(tableDataFav);
+    tableRow.appendChild(deleteButton);
     tableElement.appendChild(tableRow);
   });
 }
@@ -58,5 +72,12 @@ formElement.addEventListener('submit', function (event) {
   books.push(values);
   renderTable();
   window.localStorage.setItem('localBooksData', JSON.stringify(books));
+  {
+    formElement['title'].value = '';
+    formElement['author'].value = '';
+    formElement['read'].checked = '';
+    formElement['favorite'].checked = '';
+  }
 });
 
+console.log(books);
